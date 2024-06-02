cc669a17e6a184b6f033fd19aafbd18bc0bf783d
leon <leon@rl-book>
2024-06-02 09:15:50 +0000

    刚刚玩了自行车店的示例，学习了泊松分布，学习了通过状态转移矩阵的特征向量来获取stationary distrution，并且用graphviz来画状态转移图。

--- rl/chapter2/simple_inventory_mp.py
+++ rl/chapter2/simple_inventory_mp.py
@@ -3,6 +3,7 @@ from typing import Mapping, Dict
 from rl.distribution import Categorical, FiniteDistribution
 from rl.markov_process import FiniteMarkovProcess
 from scipy.stats import poisson
+import graphviz
 
 
 @dataclass(frozen=True)
@@ -43,11 +44,40 @@ class SimpleInventoryMPFinite(FiniteMarkovProcess[InventoryState]):
                 }
                 d[InventoryState(alpha, beta)] = Categorical(state_probs_map)
         return d
+    
+    def presentAlphaBeta(self, s: InventoryState) -> str:
+        return f'α={s.on_hand}, β={s.on_order}'
+    
+    def generate_image(self) -> graphviz.Digraph:
+        d = graphviz.Digraph()
 
+        for s in self.transition_map.keys():
+            d.node(self.presentAlphaBeta(s.state))
 
+        for s, v in self.transition_map.items():
+            for s1, p in v:
+                d.edge(self.presentAlphaBeta(s.state), self.presentAlphaBeta(s1.state), label=f"{p:.1%}")
+
+        return d
+
+import numpy as np
+import matplotlib.pyplot as plt
 if __name__ == '__main__':
     user_capacity = 2
     user_poisson_lambda = 1.0
+
+    # 生成x轴的值
+    x = np.arange(0, 10)
+
+    # 计算每个x值的PMF
+    pmf = poisson.pmf(x, user_poisson_lambda)
+
+    # 画出PMF
+    plt.bar(x, pmf)
+    plt.xlabel('Number of events (k)')
+    plt.ylabel('Probability')
+    plt.title('Poisson PMF')
+    plt.savefig('poisson.png')
 
     si_mp = SimpleInventoryMPFinite(
         capacity=user_capacity,
@@ -61,3 +91,6 @@ if __name__ == '__main__':
     print("Stationary Distribution")
     print("-----------------------")
     si_mp.display_stationary_distribution()
+
+    digraph = si_mp.generate_image()
+    digraph.render('simpleInventoryMP', format='png', cleanup=True)
